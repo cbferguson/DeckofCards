f923af7e5b33c9bbfff3783dc67bf50c31328228
Heath Kuespert <hrk601@gmail.com>
2020-04-21 07:11:21 -0500

    clean up

--- gui_test.py
+++ gui_test.py
@@ -21,15 +21,18 @@ rdeck_id = response.json()['deck_id']
 #start with 2 cards
 numcards = 2
 
+#request number of cards
 def get_cards(numofcards):
     draw = requests.get('https://deckofcardsapi.com/api/deck/' + str(rdeck_id) + '/draw/?count=' + str(numofcards))
     rcards = draw.json()
     return rcards
 
+#get one card
 def hit_me():
     card = get_image(get_cards(1))
     return card
 
+#get images for card(s)
 def get_image(url):
     image_url = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     image_byt = urlopen(image_url).read()
@@ -48,13 +51,16 @@ y = 350
 
 # use width x height + x_offset + y_offset (no spaces!)
 root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
 # this GIF picture previously downloaded to tinypic.com
-cards = get_cards(str(2))
+cards = get_cards('2')
 photo = tk.PhotoImage(data=get_image(cards['cards'][0]['image']))
 photo2 = tk.PhotoImage(data=get_image(cards['cards'][1]['image']))
+
 # create a white canvas
 cv = tk.Canvas(bg='white')
 cv.pack(side='top', fill='both', expand='yes')
+
 # put the image on the canvas with
 # create_image(xpos, ypos, image, anchor)
 cv.create_image(10, 10, image=photo, anchor='nw')
